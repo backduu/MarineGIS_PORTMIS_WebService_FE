faabde845d910983532874a34a0934ed65bea87c
backduu <power4290@gmail.com>
2026-02-12 17:12:34 +0900

    WMS를 활용해 해안선 라인 따내기
    - leaflet 라이브러리 활용하여 지도 특성 한국에 맞게 커스터마이징 처리
    - geoServer와 연동하여 postGIS 연계 및 해안선 데이터 WMS로 이미지 받아오게 처리

--- marine_mis_service/src/components/map/MapBody.jsx
+++ marine_mis_service/src/components/map/MapBody.jsx
@@ -1,3 +1,4 @@
+import L from 'leaflet';
 import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -6,30 +7,39 @@ const MapBody = () => {
   const position = [36.5, 127.5];
   const zoom = 7;
 
+  // 이동 제한 영역 설정
+  const koreaBounds = [
+      [32.0, 123.0], // 남서단
+      [40.0, 132.0], // 북동단
+  ];
+
   return (
     <main className="flex-1 relative z-0">
       <MapContainer 
         center={position} 
-        zoom={zoom} 
+        zoom={zoom}
+        minZoom={7}
+        maxZoom={18}
+        maxBounds={koreaBounds} // 이 영역 밖으로 이동 불가
+        maxBoundsViscosity={155.0} // 영역 밖으로 튕겨 나가는 강도 (1.0은 완전 고정)
         style={{ height: '100%', width: '100%' }}
       >
-        {/* 기본 OpenStreetMap 레이어 */}
+        {/* 기본 OpenStreetMap 레이어*/}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        
-        {/* GeoServer로부터 데이터를 가져오기 위한 WMS 레이어 예시 */}
-        {/* 실제 환경에서는 GeoServer URL과 레이어 이름을 설정해야 함 */}
-        {/* 
+
         <WMSTileLayer
-          url="YOUR_GEOSERVER_WMS_ENDPOINT"
-          layers="YOUR_LAYER_NAME"
-          format="image/png"
-          transparent={true}
-          attribution="GeoServer"
+          url="http://127.0.0.1:8020/geoserver/korea_coast/wms"
+          params={{
+              layers: "korea_coast:all_countries_coastline_2025",
+              format: "image/png",
+              version: "1.1.1",
+              transparent: true,
+              /*crs: L.CRS.EPSG4326*/
+          }}
         />
-        */}
       </MapContainer>
     </main>
   );
