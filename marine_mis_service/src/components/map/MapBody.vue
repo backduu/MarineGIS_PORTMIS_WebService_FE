b06c1dd6aeed88c196c90b34260d2f1f49002352
Doohyun <qorengus531@bizmeka.com>
2026-08-20 16:30:37 +0900

    add: cesium 초기 설정

--- marine_mis_service/src/components/map/MapBody.vue
+++ marine_mis_service/src/components/map/MapBody.vue
@@ -6,6 +6,7 @@ import { onMounted, ref, onBeforeUnmount } from 'vue';
 import * as Cesium from 'cesium';
 import { useMapStore } from '@/store/useMapStore';
 import ObsDetailModal from "@/components/map/ObsDetailModal.vue";
+Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
 
 const cesiumViewer = ref<Cesium.Viewer | null>(null);
 const cesiumContainer = ref<HTMLElement | null>(null);
@@ -20,14 +21,18 @@ const initCesium = () => {
   if (cesiumContainer.value) {
     cesiumViewer.value = new Cesium.Viewer(cesiumContainer.value, {
       terrain: Cesium.Terrain.fromWorldTerrain(),
-      animation: false,
-      timeline: false,
-      navigationHelpButton: false,
-      sceneModePicker: false,
-      baseLayerPicker: false,
-      geocoder: false,
+      animation: false, // 애니메이션 적용해서 시간 흐름 제어할 수 있게 함
+      timeline: false, // 타임라인 바가 나와서 특정 시간대 조회할 수 있게 함
+      navigationHelpButton: true, // 도움말 제공 (마우스 활용)
+      sceneModePicker: true, // 2D/3D 모드 변경 가능
+      baseLayerPicker: true, // 기본 레이어 선택 기능
+      geocoder: true, // 위치 검색 기능
       homeButton: false,
-      fullscreenButton: false
+      fullscreenButton: true,
+      infoBox: false, // 세슘 기본 팝업 끄기
+      selectionIndicator: false, // 클릭 시 나오는 초록색 테두리 끄기
+      // scene3DOnly:true, // 3D 모드 고정
+      creditContainer: document.createElement("div") // 크레딧 표시 영역 제거
     });
 
     // 한국 좌표로 초기 카메라 설정 (Cesium은 라디안 단위 사용)
